a3f8429fa751509bdb434bc28d4a5be4555664c9
TheJester32 <bogdanpopoveducation@gmail.com>
2023-03-08 16:04:02 +0300

    module2-task1
    Вроде все работает. Стрелочные функции и прочие сокращения я бы добавлял потом как шлифовку.

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,63 @@
 function stringLength(text, textМaxSize) {
-  if (text.length <= textМaxSize)
+
+  if (text.length <= textМaxSize){
     return true;
+  }
+
   return false;
 }
 
-stringLength('проверяемая строка', 10)
+stringLength('проверяемая строка', 10);
 
 function isStrPalindrome(str) {
-  let n = str.length;
+  const n = str.length;
   for (let i = 0; i < n / 2; i++) {
-    if (str.charAt(i) !== str.charAt(n - 1 - i))
+    if (str.charAt(i) !== str.charAt(n - 1 - i)){
       return false;
+    }
   }
   return true;
 }
 
-isStrPalindrome('топот')
+isStrPalindrome('топот');
 
-function getNum(x) {
-  const parsed = parseInt(x);
-  if (isNaN(parsed)) { return 0; }
-  return parsed;
+// eslint-disable-next-line no-unused-vars
+function getNums(str) {
+  let res = '';
+  for(let i = 0; i < str.length; i++) {
+    if (str[i] >= '0' && str[i] <= '9') {
+      res = res + str[i];
+    }
+  }
+  if (res === '') {
+    return NaN;
+  }
+  return +res;
 }
 
-getNum('2023 год')
+getNums('2023 год');
 
-function filesLocation(str, textМaxSize) {
-  if (str = str, str.length <= textМaxSize)
+function getString (n, str) {
+  let counter = 0;
+  let emptyString = '';
+  while (emptyString.length < n) {
+    emptyString = emptyString + str[counter];
+    counter++;
+    if (counter >= str.length) {
+      counter = 0;
+    }
+  }
+  return emptyString;
+}
+
+function addStringSize (str, strMinSize, additionalStr) {
+  if (str.length < strMinSize) {
+    return getString(strMinSize - str.length, additionalStr) + str;
+  } else {
     return str;
-    alert ('Error')
+  }
 }
 
-filesLocation('диск D', 10)
+addStringSize('q', 4, 'werty');
+
+
